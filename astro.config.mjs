cacec8df1fcb3824326129a80aa427d6f7e6d1ef
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2026-06-04 13:51:08 +0530

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   // If repo is named "yash2700.github.io" → base: '/'
   // If repo is named "portfolio" → base: '/portfolio'
-  site: 'https://yash2700.github.io',
+  site: 'https://yaswanth-reddy.github.io',
   base: '/',
   output: 'static',
   compressHTML: true,
+  build: {
+    assets: 'assets',
+  },
 });
